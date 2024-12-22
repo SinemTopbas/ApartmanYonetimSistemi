535a8d3f3bf09821fe75c594fe92c6799bef22c4
Sinem Topbas <127394173+SinemTopbas@users.noreply.github.com>
2024-12-22 13:06:20 +0300

    Update Observer.java

--- Observer.java
+++ Observer.java
@@ -4,32 +4,25 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 interface Observer {
-    
     void update(String message);
 }
-
 
 class ResidentObserver implements Observer {
     private int userId; 
     private String username; 
 
-    
-    public ResidentObserver(int userId, String username) {
+   public ResidentObserver(int userId, String username) {
         this.userId = userId;
         this.username = username;
     }
-
-    
+ 
     public int getUserId() {
         return userId;
     }
 
-    
     @Override
     public void update(String message) {
-
         System.out.println("Bildirim " + username + ": " + message);
     }
 }
